92e093cb4ddecb5a863c0187c5953da437541dc2
iubizi <106417012+iubizi@users.noreply.github.com>
2022-06-13 17:43:24 -0400

    Add files via upload

---
+++ download.py
@@ -0,0 +1,12 @@
+import os
+os.system('speedtest-cli')
+print()
+
+
+# pip install speedtest-cli
+import speedtest
+st = speedtest.Speedtest()
+
+while True:
+    print(st.download())
+    # print(st.upload())
